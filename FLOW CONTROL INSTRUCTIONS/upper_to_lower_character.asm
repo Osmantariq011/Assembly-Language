4729976ded3b4eba055f2f23a72d876c794065bf
Osman Tariq <119866607+Osmantariq011@users.noreply.github.com>
2023-06-05 07:20:10 -0700

    Create upper_to_lower_character.asm
    converts each input character from the keyboard to lowercase. The program ends when ENTER is pressed

---
+++ FLOW CONTROL INSTRUCTIONS/upper_to_lower_character.asm
@@ -0,0 +1,40 @@
+org 100h
+.model small
+.stack 100h
+.data
+var1 db ?
+msg1 db 10,13,' '
+.code
+main proc    
+mov ax,@data
+mov ds,ax  
+l1:
+mov ah,1
+int 21h  
+mov bl,al
+add bl,32
+mov dl,10 
+mov ah,2
+int 21h 
+mov dl,13
+mov ah,2
+int 21h
+mov dl,bl
+mov ah,2
+int 21h  
+cmp dl,237  
+mov dl,10 
+         mov ah,2
+int 21h 
+mov dl,13
+mov ah,2
+int 21h
+jne l1   
+mov dl,msg1
+mov ah,2
+int 21h   
+mov ah,4ch
+int 21h     
+main endp
+end main
+ret
